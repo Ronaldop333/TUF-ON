a6e3ed71ada3730e8ef80d5a3c2c80fc0a0f2b62
Ronaldop333 <35382410+Ronaldop333@users.noreply.github.com>
2025-10-17 19:59:47 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// postcss.config.js
+
 module.exports = {
   plugins: {
+    // Utiliza Tailwind CSS como motor de utilitários de estilos
     tailwindcss: {},
-    autoprefixer: {},
-  },
+
+    // Adiciona prefixos automáticos de CSS para compatibilidade entre navegadores
+    autoprefixer: {}
+  }
 }
