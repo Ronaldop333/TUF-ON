2ecdfca21207dbfdb29d05f774e91ce21423dbbf
Ronaldop333 <35382410+Ronaldop333@users.noreply.github.com>
2025-10-17 19:54:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
-    domains: ['localhost'],
+    domains: [
+      'localhost',
+      'tuf-on.com',
+      'images.unsplash.com',
+      'cdn.nasa.gov',
+      'apod.nasa.gov',
+      'upload.wikimedia.org'
+    ],
+  },
+
+  experimental: {
+    appDir: true,
+  },
+
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en',
   },
 }
 
